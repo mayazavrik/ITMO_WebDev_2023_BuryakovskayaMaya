bdb56a424b6e3de7f2db4a7e165a8255a8166332
mayazavrik <mayazavrik@gmail.com>
2023-04-06 22:00:27 +0300

    13 lesson class version

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
 import "uno.css";
 import "@unocss/reset/tailwind.css";
+
+const domBtnCreateTask = document.getElementById("btnCreateTask");
+const domPopupCreateTask= document.getElementById("popupCreateTask");
+const domBtnCloseCreateTaskPopup= document.getElementById("btnCloseCreateTaskPopup");
+
+domBtnCreateTask.onclick = () => {
+    console.log("click");
+    domPopupCreateTask.classList.remove("hidden");
+    domBtnCloseCreateTaskPopup.onclick = () => {
+        domPopupCreateTask.classList.add("hidden");
+        domBtnCloseCreateTaskPopup.onclick = null;
+    }
+};
