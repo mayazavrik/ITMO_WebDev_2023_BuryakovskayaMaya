74867f6ddc52285d6fe0b74782ee24a0e86567e9
mayazavrik <mayazavrik@gmail.com>
2023-04-11 12:48:23 +0300

    final version

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
+import "./script.js"
 import "uno.css";
-import "@unocss/reset/tailwind.css";
-import DOM from "./src/constants/dom";
-import {randomString} from "./src/utils/stringUtils";
+// import "@unocss/reset/tailwind.css";
+
+
 
--- script.js
+++ script.js
@@ -1,31 +1,88 @@
 
-let x = ``;
-let y = ``;
-let sign = ``;
+let x = "";
+let y = "";
+let sign = "";
+let finish = false;
 
-const number = [`0`,`1`,`2`,`3`,`4`,`5`,`6`,`7`,`8`,`9`,`.`];
-const signs = [`+`,`-`,`X`,`/`];
-const output = document.querySelector(`.calc_result`);
-function clear () {
-out.content = 0;
+const number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+const action = ["+", "-", "X", "/"];
+const buttons = document.querySelector(".calc");
+const output = document.querySelector(".calc_result");
+function clearAll() {
+  x = "";
+  y = "";
+  sign = "";
+  finish = false;
+  output.innerText = '';
 }
-document.querySelector(`btn_reset`).onclick = clearAll;
-document.querySelector('.calc').onclick =(event) => {
-    if(!event.target.classList.contains('btn')) return;
-    if(event.target.classList.contains(`btn_reset`)) return;
-    out.text = '';
-    const data = event.target.content;
-    if (number.includes(data)) {
-        x = x + data;
-        console.log ('x, y, sign');
-        out.text = x;
+
+function buttonClick(event) {
+
+  if (!event.target.classList.contains("calc_btn")) return;
+  if (event.target.classList.contains("calc_btn--reset")) return;
+  output.innerText = "";
+  const data = event.target.innerText;
+
+  if (number.includes(data)) {
+    if (y === "" && sign === "") {
+      x += data;
+      console.log(x, y, sign);
+      output.innerText = x;
+    } else if (x !== "" && y !== "" && finish) {
+      y = data;
+      finish = false;
+      output.innerText = y;
+    } else {
+      y += data;
+      output.innerText = y;
+    }
+    console.log(x, y, sign);
+    return;
+  }
+
+  if (action.includes(data)) {
+    sign = data;
+    output.text = sign;
+    console.table("x, y, sign");
+    return;
+  }
+  if (action.includes(data)) {
+    sign = data;
+    output.innerText = sign;
+    console.table("x, y, sign");
+    return;
+  }
+
+  if (data === "=") {
+    switch (sign) {
+      case "+":
+        x = +x + +y;
+        break;
+      case "-":
+        x = x - y;
+        break;
+      case "X":
+        x = x * y;
+        break;
+      case "/":
+        if (y === "0") {
+          output.innerText = "error";
+          x = "";
+          y = "";
+          sign = "";
+          return;
+        }
+        x = x / y;
+        break;
+        
+        default:
+          return;
     }
 
-    if (signs.includes(data)) {
-        sign=data;
-        out.text = sign;
-        console.log('x, y, sign');
-        return;
-    }
-
-} 
+    finish = true;
+    output.innerText = x;
+    console.table(x, y, sign);
+  }
+}
+document.querySelector(".calc_btn--reset").onclick = clearAll;
+document.querySelector(".calc").onclick = buttonClick;
