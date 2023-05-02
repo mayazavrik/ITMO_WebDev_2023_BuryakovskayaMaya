c56e17a261ec2d8a9f157e8d7b9417578ba7b5b6
mayazavrik <127432774+mayazavrik@users.noreply.github.com>
2023-05-02 13:22:20 +0300

    probably final, surge

--- main.js
+++ main.js
@@ -1,65 +1,76 @@
 let BODY = document.querySelector('body');
-//возвращает первый найденный элемент с тэгом body?или что это? 
-
 function addBlock(width, height, row) {
-  //создаем функцию, в которую передаем 3 параметра
-  let logo = document.createElement('div');
-  //создаем переменную, которая создает див
-  logo.style.width = `${width}px`;
-  logo.style.height = `${height}px`;
-  //создаем две переменные дальше непонятно
+ 
+  let changeColor = document.createElement('div');
+  changeColor.style.width = `${width}px`;
+  changeColor.style.height = `${height}px`;
+  
   let randomBackground = Math.floor(Math.random() * 2);
-  //создаем переменную где возвращается число с плавающей точкой от 0 до 1,
-  //умноженное на 2. и округленное в меньшую сторону
+  
   if (randomBackground == 1) {
-    logo.style.background = `#000`;
-    //если значение переменной 1 то создаем переменную с цветом 0
+    changeColor.style.background = `#000`;
+    
   } else {
-    logo.style.background = `#eee`;
-    //в другом случае с цветом е
+    changeColor.style.background = `#eee`;
   }
 
-  row.appendChild(logo);
-  //вставляем в конец row(где он?) значение logo
+  row.appendChild(changeColor);
 }
 
-function rows(width, height, column, counterBlocks) {
-  //создаем функцию, в которую передаем 4 параметра
+function cubes(width, height, column, counterBlocks) {
   let row = document.createElement('div');
-  //создаем переменную, которая создает див
+  
   row.style.width = `${width}px`;
   row.style.height = `${height}px`;
   row.style.display = 'flex';
-  while (counterBlocks--) {
-    //не понимаю. вроде уменьшение на один, но где условие
-    addBlock(20, 20, row);
+  
+
+  let i = 0;
+  while(i < counterBlocks ) {
+    i++;
+    let width = 20;
+    let height = 20;
+    addBlock(width, height, row);
+    
   }
-  let row2 = row.cloneNode(true);
-  row2.style.flexDirection = 'row-reverse';
+  let rowMirror = row.cloneNode(true);
+  rowMirror.style.flexDirection = 'row-reverse';
   column.appendChild(row);
-  column.appendChild(row2);
+  column.appendChild(rowMirror);
 }
 
-function columns(width, height, counterBlocks, block) {
+function smallRows (width, height, counterBlocks, block) {
+
   let column = document.createElement('div');
+ 
   column.style.width = `${width}px`;
   column.style.height = `${height}px`;
+  
   column.style.display = 'flex';
   block.appendChild(column);
-  rows(60, 20, column, counterBlocks);
+  
+  cubes(60, 20, column, counterBlocks);
 }
 
-function mainBlock(counterColumns, counterBlocks) {
+function mainBlock(countersmallRows , counterBlocks) {
   let block = document.createElement('div');
   BODY.appendChild(block);
-  while (counterColumns--) {
-    columns(100, 20, counterBlocks, block);
-  }
+  let i = 0;
+  while(i < countersmallRows ) {
+    i++;
+    let width = 100;
+    let height = 20;
+    smallRows (width, height, counterBlocks, block);
+  } 
 }
 
 function AllCartoons(count) {
-  while (count--) {
-    mainBlock(8, 4);
-  }
+  let width = 8;
+  let height= 4;
+  let i = 0;
+  while(i < count) {
+    i++;
+    mainBlock(width, height);
+  } 
 }
 AllCartoons(9);
