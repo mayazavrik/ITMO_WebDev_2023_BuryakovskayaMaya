5f7331b08cfd42eac93db4a1d91266cdb194a4c9
mayazavrik <127432774+mayazavrik@users.noreply.github.com>
2023-05-13 20:17:51 +0300

    27-vue-basic-commit

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,54 @@ import MyName from './components/MyName.vue';
 </script>
 
 <template>
+  <select 
+    name="status" 
+    @change= "onSelectChange">
+    <option
+    v-if="!status"
+    value="unselected"
+    selected
+    >
+      Unselected
+    </option>
+  <option 
+    v-for="item in ['Ready', 'Start', 'Stop']"
+    :key ="item"
+    :value="item"
+    :selected="item ==="
+   >
+   {{ item }}
+
+    
+  </option>
   
-  <MyName/>
+</select>
+
+  <MyName 
+  :status="status"
+  @reset="onStatusReset"
+  />
   
 </template>
+
+<script>
+export default {
+  data: () => ({
+    status: 'Loading'
+  }),
+  mathods: {
+    onSelectChange(event) {
+      console.log('> App -> onSelectChange:', event.target.value);
+      this.status = event.target.value;
+
+    },
+    onStatusReset() {
+      console.log('>App -> onStatusReset');
+      this.status = null;
+    }
+  }
+};
+</script>
 
 <style scoped>
 
--- src/components/MyName.vue
+++ src/components/MyName.vue
@@ -4,16 +4,56 @@
      <span style="color: red;">
       {{ lastName }}</span>
       </div>
+      <small>
+        <span>
+            Status:
+        </span>
+        <span :class="{
+            'selected': !!status,
+            'unselected': !status
+        }"
+        >
+            Status:
+        </span>
+        <span>{{ status }}</span>
+        <div>
+            <button
+            :style="{
+                visibility: status ? 'visible' : 'hidden'
+            }"
+             @click="onResetClick"
+             >Reset</button>
+        </div>
+    </small>
 </template>
 
 <script>
 export default {
-    name: "MyName",
+    name: 'MyName',
+    props: {
+        status: {
+            type: String,
+            default: '-'
+        },    
+    },
+    emits: ['reset'],
     data: () => ({
         firstName: 'Maya',
         lastName: 'Zavrik'
-    })
+    }),
+    methods: {
+        onResetClick() {
+            console.log('> MyName -> onResetClick');
+            this.$emit('reset');
+        }
+    }
 };
 </script>
 <style scoped>
-</style><a href="tel:"></a>
+.selectes {
+    color: green;
+}
+.unselected {
+    color: red;
+}
+</style>
