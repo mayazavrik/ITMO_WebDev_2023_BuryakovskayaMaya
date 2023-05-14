50ff7b210f09dc423609d68afadbd5aae27a5e44
mayazavrik <127432774+mayazavrik@users.noreply.github.com>
2023-05-14 21:13:11 +0300

    28-final

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,32 @@
+<template>
+<div>
+    {{ index }}) {{ item }}
+    <button @click="$emit('delete')">
+      x
+    </button>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'TodoItem',
+    props: {
+        index: {
+            type: Number,
+            default: 0
+        },
+        text: {
+            type: String,
+            default: '-'
+        }
+    },
+    emits: ['delete'],
+    unmonted() {
+        console.log('> TodoItem -> ')
+    }
+    
+};
+</script>
+
+<style scoped>
+</style>
