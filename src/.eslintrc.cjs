df71766d50842bcd13e33b8a665cdbfd26ee9e89
mayazavrik <127432774+mayazavrik@users.noreply.github.com>
2023-05-31 16:00:29 +0300

    32-final

---
+++ src/.eslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+    extends: [
+      // add more generic rulesets here, such as:
+      // 'eslint:recommended',
+      'plugin:vue/vue3-recommended',
+      // 'plugin:vue/recommended' // Use this if you are using Vue.js 2.x.
+    ],
+    rules: {
+      // override/add rules settings here, such as:
+      // 'vue/no-unused-vars': 'error'
+      semi: ["error", "always"],
+      quotes: ["error", "single"],
+      "vue/max-attributes-per-line": ["error", {
+        "singleline": {
+          "max": 1
+        },
+        "multiline": {
+          "max": 1
+        }
+      }]
+    }
+  }
