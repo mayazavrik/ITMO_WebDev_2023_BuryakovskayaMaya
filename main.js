03a99cab08f8bd05931abe5826af0cd0139fe641
mayazavrik <127432774+mayazavrik@users.noreply.github.com>
2023-06-07 16:59:53 +0300

    popup_add

--- main.js
+++ main.js
@@ -2,47 +2,18 @@ import 'uno.css';
 import '@unocss/reset/tailwind.css';
 
 
-
-const KEY_LOCAL_INVOICE = 'invoice';
-
-const getDOM = (id) => document.getElementById(id);
-const QUERY = (container, id) => container.querySelector(`[data-id="${id}"]`);
-
-const domTemplateWorkItems = getDOM(DOM.Template.ITEMS);
-const domTableWorkItems = domTemplateWorkItems.parentNode;
-domTemplateWorkItems.removeAttribute('id');
-domTemplateWorkItems.remove();
-
-const rawInvoice = localStorage.getItem(KEY_LOCAL_INVOICE);
-
-const domInvoiceId = getDOM(DOM.INVOICE_INPUT.NUMBER);
-
-const invoiceIdMask = [/\D/, /\D/, /\D/, /\D/];
-const invoicePercentMask = [/\D/, /\D/];
-
-const invoice = JSON.parse(rawInvoice);
-
-if (invoice) {
-  domInvoiceId.value = invoice.id
+var modal = document.getElementById('myModal');
+var btn = document.getElementById("btnAddItem");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function () {
+  modal.style.display = "block";
+}
+span.onclick = function() {
+  modal.style.display = "none";
 }
 
-console.log('> invoice:', invoice, typeof invoice);
-
-domInvoiceId.addEventListener('input', (e) => maskForNum(e, invoiceIdMask));
-domInvoiceId.addEventListener("blur", (e) => {
-    if (domInvoiceId.value) {
-      const invoiceId = domInvoiceId.value
-      console.log('invoiceId = ', invoiceId)
-      const invoiceVO = new InvoiceVO(invoiceId)
-      saveInvoice(invoiceVO)
-    } else {
-      window.alert('Enter the invoice number')
-    }
+window.onclick = function(event) {
+  if(event.target == modal) {
+    modal.style.display = "none";
   }
-)
-
-
-function saveInvoice(x) {
-  localStorage.setItem(KEY_LOCAL_INVOICE, JSON.stringify(x));
-  console.log('Invoice written to localStorage ', x)
 }
